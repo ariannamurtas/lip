9af1707a98306040c7b0bd98b98c4747cd11c7c4
ariannamurtas <ariannamurtas01@gmail.com>
2024-11-20 19:20:34 +0100

    arithexpr completato

---
+++ expr/arithexpr/lib/main.ml
@@ -0,0 +1,104 @@
+open Ast
+
+type exprval = Bool of bool | Nat of int
+
+let rec string_of_expr = function
+    True -> "True"
+  | False -> "False"
+  | If(e0,e1,e2) -> "If(" ^ (string_of_expr e0) ^ "," ^ (string_of_expr e1) ^ "," ^ (string_of_expr e2) ^ ")"
+  | Not(e0) -> "Not" ^ (string_of_expr e0)
+  | And(e0,e1) -> (string_of_expr e0) ^ "and" ^ (string_of_expr e1)
+  | Or(e0,e1) -> (string_of_expr e0) ^ "or" ^ (string_of_expr e1)
+  | Zero -> "0"
+  | Succ(e0) -> "succ" ^ (string_of_expr e0)
+  | Pred(e0)-> "pred" ^ (string_of_expr e0)
+  | IsZero(e0)-> "iszero" ^ (string_of_expr e0)
+
+
+
+let parse (s : string) : expr =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+exception NoRuleApplies
+
+let rec is_nv nv = match nv with
+  | Zero -> true
+  | Succ e1 -> is_nv e1
+  | _-> false
+
+(* small step *)
+let rec trace1 = function
+    If(True,e1,_) -> e1
+  | If(False,_,e2) -> e2
+  | If(e0,e1,e2) -> If (trace1 e0,e1,e2)
+  | Not(False) -> True
+  | Not(True) -> False
+  | Not(e0) -> Not(trace1 e0)
+  | And(True, True) -> True
+  | And(False,_) -> False
+  | And(_, False) -> False
+  | And(e0, e1) -> And(trace1 e0, trace1 e1)
+  | Or(False, False) -> False
+  | Or(True,_) -> True
+  | Or(_,True) -> True
+  | Or(e0,e1) -> Or(trace1 e0, trace1 e1)
+  | Succ(e0) -> Succ(trace1 e0)
+  | Pred( Succ(e0)) -> e0
+  | Pred(e0) -> Pred(trace1 e0)
+  | IsZero Zero -> True
+  | IsZero(Succ _) -> False
+  | IsZero(e0) -> IsZero(trace1 e0)
+  | _ -> raise NoRuleApplies
+
+let rec trace e = try
+    let e' = trace1 e
+    in e::(trace e')
+  with NoRuleApplies -> [e]
+
+(* big step *)
+let rec eval = function
+    True -> Bool true
+
+  | False -> Bool false
+
+  | If(e0,e1,e2) -> 
+    (match eval e0 with
+    | Bool true -> eval e1
+    | Bool false -> eval e2
+    | _-> failwith "Not Bool")
+
+  | Not(e0) -> 
+    (match eval e0 with
+    | Bool e0 -> Bool (not e0)
+    | _-> failwith "Not Bool")
+
+  | And(e0,e1) ->
+    (match eval e0, eval e1 with
+    | Bool e0, Bool e1 -> Bool(e0 && e1)
+    | _-> failwith "Not Bool")
+
+  | Or(e0,e1) -> 
+    (match eval e0, eval e1 with
+    | Bool e0, Bool e1 -> Bool(e0 || e1)
+    |_-> failwith "Not Bool")
+
+  | Zero -> Nat 0
+
+  | Succ(e0) -> 
+    (match eval e0 with
+    | Nat e0 -> Nat(e0+1)
+    | _-> failwith "Not Nat")
+
+  | Pred(e0)->
+    (match eval e0 with
+    | Nat e0 -> if e0>0 then Nat(e0 - 1) else failwith "Nat is zero"
+    | _-> failwith "Not Nat")
+
+  | IsZero(e0) ->
+    (match eval e0 with
+    | Nat 0 -> Bool true
+    | Nat n -> if n>0 then Bool false else failwith "Cannot solve"
+    | _-> failwith "Not Nat")
+
+  
